6835065fbf164283ce0df275c56f1c6bedb1d9b1
Martin <martin.ukhanov@gmail.com>
2022-10-19 18:40:32 -0400

    Added Poppins font

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,5 +1,19 @@
 import { createGlobalStyle } from 'styled-components';
+import poppinsWoff from './fonts/Poppins/Poppins.woff';
+import poppinsWoff2 from './fonts/Poppins/Poppins.woff2';
+import poppinsTtf from './fonts/Poppins/Poppins.ttf';
 
-const GlobalStyle = createGlobalStyle``;
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Poppins';
+    src: url(${poppinsWoff}) format('woff'),
+      url(${poppinsWoff2}) format('woff2'),
+      url(${poppinsTtf}) format('truetype');
+  }
+
+  body {
+    font-family: 'Poppins', sans-serif;
+  }
+`;
 
 export default GlobalStyle;
