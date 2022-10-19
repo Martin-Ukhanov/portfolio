bdef509d28c9bf5852d48f9ed349010e7651b471
Martin <martin.ukhanov@gmail.com>
2022-10-18 22:52:23 -0400

    Created icon components

---
+++ src/components/icons/MenuIcon.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function MenuIcon() {
+  return (
+    <svg
+      className="icon"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="4 6 16 12"
+    >
+      <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z" />
+    </svg>
+  );
+}
+
+export default MenuIcon;
