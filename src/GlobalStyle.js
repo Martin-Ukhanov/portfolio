9c98b344e05c2031c1f2c2de1b7f39f03935bd59
Martin <martin.ukhanov@gmail.com>
2022-10-19 19:08:53 -0400

    Added global styles

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -11,8 +11,85 @@ const GlobalStyle = createGlobalStyle`
       url(${poppinsTtf}) format('truetype');
   }
 
+  :root {
+    --color-1: #000000;
+    --color-2: #FFFFFF;
+    --spacing: 15px;
+    --border-width: 2px;
+    --menu-width: 200px;
+  }
+
+  html {
+    scroll-behavior: smooth;
+    -webkit-tap-highlight-color: transparent;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    transition: color 0.2s;
+    transition: background-color 0.2s;
+  }
+
   body {
     font-family: 'Poppins', sans-serif;
+  }
+
+  p {
+    font-size: 1.1rem;
+    line-height: 1.5;
+  }
+
+  a {
+    font-size: 1.1rem;
+    color: inherit;
+    text-decoration: none;
+  }
+
+  button {
+    color: inherit;
+    background: none;
+    border: none;
+    outline: none;
+    font-family: inherit;
+    font-size: 100%;
+    cursor: pointer;
+  }
+
+  section {
+    width: 100%;
+    max-width: 1000px;
+    min-height: 100vh;
+    padding: var(--spacing);
+  }
+
+  .icon {
+    display: block;
+  }
+
+  .section-heading {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    font-size: 3rem;
+    text-align: center;
+  }
+
+  .section-heading::before {
+    content: '';
+    flex: 1;
+    margin-right: var(--spacing);
+    border-bottom: var(--border-width) solid var(--color-1);
+  }
+
+  .section-heading::after {
+    content: '';
+    flex: 1;
+    margin-left: var(--spacing);
+    border-bottom: var(--border-width) solid var(--color-1);
   }
 `;
 
