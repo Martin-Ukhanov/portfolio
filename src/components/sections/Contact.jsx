402de5bea2cdb0e10f42e3d4b39b4491a4272459
Martin <martin.ukhanov@gmail.com>
2022-10-19 19:19:23 -0400

    Removed font weight style from submit button

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -41,7 +41,6 @@ const StyledContact = styled.section`
   .contact-form button {
     padding: var(--spacing);
     border: var(--border-width) solid var(--color-1);
-    font-weight: normal;
   }
 
   .contact-form button:hover,
