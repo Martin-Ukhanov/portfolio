ba4e37678831fc52ee6546a6f1c6e584bd86af25
Martin <martin.ukhanov@gmail.com>
2022-10-19 17:54:36 -0400

    Created contact section component

---
+++ src/components/sections/Contact.jsx
@@ -0,0 +1,96 @@
+import React, { useRef } from 'react';
+import styled from 'styled-components';
+import emailjs from '@emailjs/browser';
+
+const StyledContact = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  .contact-form {
+    display: flex;
+    flex-direction: column;
+    row-gap: var(--spacing);
+  }
+
+  .contact-form .info {
+    display: flex;
+    gap: var(--spacing);
+  }
+
+  .contact-form input,
+  .contact-form textarea {
+    width: 100%;
+    padding: var(--spacing);
+    color: inherit;
+    background-color: inherit;
+    border: var(--border-width) solid var(--color-1);
+    border-radius: 0;
+    outline: none;
+    font-size: 100%;
+    font-family: inherit;
+    resize: none;
+  }
+
+  .contact-form input::placeholder,
+  .contact-form textarea::placeholder {
+    color: var(--colorl-2);
+    opacity: 0.7;
+  }
+
+  .contact-form button {
+    padding: var(--spacing);
+    border: var(--border-width) solid var(--color-1);
+    font-weight: normal;
+  }
+
+  .contact-form button:hover,
+  .contact-form button:active {
+    color: var(--color-2);
+    background-color: var(--color-1);
+  }
+
+  @media screen and (max-width: 550px) {
+    .contact-form .info {
+      flex-direction: column;
+    }
+  }
+`;
+
+function Contact() {
+  const contactFormRef = useRef(null);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      'contact_service',
+      'contact_form',
+      contactFormRef.current,
+      '***REMOVED***'
+    );
+
+    contactFormRef.current.reset();
+  };
+
+  return (
+    <StyledContact id="contact">
+      <div className="section-heading">Contact</div>
+      <form
+        className="contact-form"
+        autoComplete="off"
+        ref={contactFormRef}
+        onSubmit={sendEmail}
+      >
+        <div className="info">
+          <input type="text" name="user_name" placeholder="Name" required />
+          <input type="email" name="user_email" placeholder="Email" required />
+        </div>
+        <textarea name="message" rows="12" placeholder="Message" required />
+        <button type="submit">Submit</button>
+      </form>
+    </StyledContact>
+  );
+}
+
+export default Contact;
