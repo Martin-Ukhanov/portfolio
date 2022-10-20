3535477e03bd2d9de4357b5187c3e2add4358977
Martin <martin.ukhanov@gmail.com>
2022-10-20 18:38:05 -0400

    Replaced p blocks with div blocks and added text styling

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -11,6 +11,8 @@ const StyledAbout = styled.section`
     display: flex;
     flex-direction: column;
     row-gap: var(--spacing);
+    font-size: var(--fz-xs);
+    line-height: 1.5;
     text-align: justify;
   }
 
@@ -30,21 +32,21 @@ function About() {
     <StyledAbout id="about">
       <div className="section-heading">About</div>
       <div className="text">
-        <p>
+        <div>
           Hi, my name is Martin and I love building things for the web. Equipped
           with a deep knowledge of programming and an eye for design, I strive
           to create exceptional digital experiences.
-        </p>
-        <p>
+        </div>
+        <div>
           I first started my coding journey during the pandemic learning how to
           create simple games. This sparked my love and passion for software
           development. Since then I&apos;ve spent countless hours studying and
           refining my skills in order to become the developer I am today.
-        </p>
-        <p>
+        </div>
+        <div>
           I&apos;m currently working towards a degree in computer science and am
           teaching myself web development on the side.
-        </p>
+        </div>
       </div>
       <div className="tags">
         <div>&lt;Developer&gt;</div>
